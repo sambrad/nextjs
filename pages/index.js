d83a04dd3fd0d3a78d97b1726ece9841555e8daf
sammbrad <63823120+sammbrad@users.noreply.github.com>
2020-09-09 17:40:32 -0400

    picFinally

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
       </div>
 
       <main className={styles.main}>
-        <img src="/headshot.JPG" style={{ width: '10rem',
+        <img src="/headshot.JPG" style={{ width: '14rem',
                                           height: '14rem',
                                           border: "5px solid black",
                                           borderRadius: '50%',
